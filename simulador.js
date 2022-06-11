a1ab195428c5218469b822d59b3d8c12cd0041e4
Esteban Landucci <emlanducci@gmail.com>
2022-06-11 14:05:39 -0300

    Filtro por género agregado correctamente

--- simulador.js
+++ simulador.js
@@ -1,107 +1,111 @@
 // ARRAY DE OBJETOS
 const catalogo = [
-    {titulo: 'El aleph', precio: 3099, genero: cuentos},
-    {titulo: 'Ficciones', precio: 3099, genero: cuentos},
-    {titulo: 'Inquisiciones-Otras inquisiciones', precio: 3799, genero: ensayo},
-    {titulo: 'La rosa profunda', precio: 2049, genero: poesia},
-    {titulo: 'Historia de la eternidad', precio: 2049, genero: ensayo},
-    {titulo: 'La cifra', precio: 2049, genero: poesia},
+    {id: 1, titulo: 'El aleph', precio: 3099, genero: "cuentos"},
+    {id: 2, titulo: 'Ficciones', precio: 3099, genero: "cuentos"},
+    {id: 3, titulo: 'Inquisiciones-Otras inquisiciones', precio: 3799, genero: "ensayo"},
+    {id: 4, titulo: 'La rosa profunda', precio: 2049, genero: "poesia"},
+    {id: 5, titulo: 'Historia de la eternidad', precio: 2049, genero: "ensayo"},
+    {id: 6, titulo: 'La cifra', precio: 2049, genero: "poesia"},
 ]
 
-// FUNCIÓN CONSTRUCTORA PARA REALIZAR PEDIDO
-class Pedido {
-    constructor (producto, precio, cantidad) {
-        this.producto = producto,
-        this.precio = precio,
-        this.cantidad = cantidad,
-        this.descuento = 0,
-        this.envio = 0,
-        this.subTotal = 0,
-        this.total =0
-    }
+const filtrarEnsayos = catalogo.filter ( (elemento) => elemento.genero.includes ("ensayo") );
+console.log (filtrarEnsayos);
 
-calcularSubTotal() {
-    this.subTotal = this.precio * this.cantidad;
-}
 
-calcularEnvio () {
-    if (this.cantidad >= 2) {
-        this.envio = 0;
-    } else {
-        this.envio = 380;
-    }
-}
+// // FUNCIÓN CONSTRUCTORA PARA REALIZAR PEDIDO
+// class Pedido {
+//     constructor (producto, precio, cantidad) {
+//         this.producto = producto,
+//         this.precio = precio,
+//         this.cantidad = cantidad,
+//         this.descuento = 0,
+//         this.envio = 0,
+//         this.subTotal = 0,
+//         this.total =0
+//     }
 
-calcularDescuento () {
-    this.descuento = 15 * this.subTotal / 100;
-    return this.descuento;
-    }
+// calcularSubTotal() {
+//     this.subTotal = this.precio * this.cantidad;
+// }
 
-calcularTotal () {
-    this.total = parseFloat(this.subTotal + this.envio - this.descuento);
-    return this.total;
-    }
-}
+// calcularEnvio () {
+//     if (this.cantidad >= 2) {
+//         this.envio = 0;
+//     } else {
+//         this.envio = 380;
+//     }
+// }
 
-// FUNCIÓN PARA HACER UN PEDIDO
-const pedidoProducto = () => {
-    let producto = 0;
-    let cantidadProducto = 0;
-    let precio = 0;
+// calcularDescuento () {
+//     this.descuento = 15 * this.subTotal / 100;
+//     return this.descuento;
+//     }
 
-while (producto == 0 || producto > 5 || !producto) {
-    producto = parseInt ( prompt ("¿Qué libro de Jorge Luis Borges quieres comprar?: 1) El Aleph $2149 2) Ficciones $2149 3) Historia universal de la infamia $1449 4) El informe de Brodie $1449 5) Cuentos completos $2699"));
-}
+// calcularTotal () {
+//     this.total = parseFloat(this.subTotal + this.envio - this.descuento);
+//     return this.total;
+//     }
+// }
 
-switch (producto) {
-    case 1:
-        producto = "El aleph";
-        precio = 2149;
-        break;
+// // FUNCIÓN PARA HACER UN PEDIDO
+// const pedidoProducto = () => {
+//     let producto = 0;
+//     let cantidadProducto = 0;
+//     let precio = 0;
 
-    case 2:
-        producto = "Ficciones";
-        precio = 2149;
-        break;
+// while (producto == 0 || producto > 5 || !producto) {
+//     producto = parseInt ( prompt ("¿Qué libro de Jorge Luis Borges quieres comprar?: 1) El Aleph $2149 2) Ficciones $2149 3) Historia universal de la infamia $1449 4) El informe de Brodie $1449 5) Cuentos completos $2699"));
+// }
 
-    case 3:
-        producto = "Historia universal de la infamia";
-        precio = 1449;
-        break;
+// switch (producto) {
+//     case 1:
+//         producto = "El aleph";
+//         precio = 2149;
+//         break;
 
-    case 4:
-        producto = "El informe de Brodie";
-        precio = 1449;
-        break;
+//     case 2:
+//         producto = "Ficciones";
+//         precio = 2149;
+//         break;
 
-    case 5:
-        producto = "Cuentos completos";
-        precio = 2699;
-        break;
-}
+//     case 3:
+//         producto = "Historia universal de la infamia";
+//         precio = 1449;
+//         break;
 
-while (cantidadProducto == 0 || !cantidadProducto) {
-    cantidadProducto = parseInt (prompt ("Elegiste el título: "+ producto + "¿Cuántos ejemplares quieres?"));
-}
+//     case 4:
+//         producto = "El informe de Brodie";
+//         precio = 1449;
+//         break;
 
-const compra = new Pedido (producto, precio, cantidadProducto);
+//     case 5:
+//         producto = "Cuentos completos";
+//         precio = 2699;
+//         break;
+// }
 
-return compra;
+// while (cantidadProducto == 0 || !cantidadProducto) {
+//     cantidadProducto = parseInt (prompt ("Elegiste el título: "+ producto + "¿Cuántos ejemplares quieres?"));
+// }
 
-};
+// const compra = new Pedido (producto, precio, cantidadProducto);
 
-alert ("Bienvenid@ a la librería");
+// return compra;
 
-const pedido = pedidoProducto ();
+// };
 
-pedido.calcularSubTotal();
-pedido.calcularEnvio();
-pedido.calcularDescuento()
-pedido.calcularTotal();
+// alert ("Bienvenid@ a la librería");
 
-alert ("Detalle del pedido:\n\n"+
-    "- "+pedido.producto+ " x " +pedido.cantidad+ ": $"+pedido.precio * pedido.cantidad +"\n" +
-    "- Descuento 15%:  $" +pedido.calcularDescuento()+  "\n" +
-    "- Costo de envío: $"+pedido.envio+ "\n\n" +
-    "Total = $" +pedido.total
-);
+// const pedido = pedidoProducto ();
+
+// pedido.calcularSubTotal();
+// pedido.calcularEnvio();
+// pedido.calcularDescuento()
+// pedido.calcularTotal();
+
+// alert ("Detalle del pedido:\n\n"+
+//     "- "+pedido.producto+ " x " +pedido.cantidad+ ": $"+pedido.precio * pedido.cantidad +"\n" +
+//     "- Descuento 15%:  $" +pedido.calcularDescuento()+  "\n" +
+//     "- Costo de envío: $"+pedido.envio+ "\n\n" +
+//     "Total = $" +pedido.total
+// );
